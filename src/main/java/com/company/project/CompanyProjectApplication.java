e2576a4dc70c5897cb0120069c211d579f8778af
wenbin <1104302665@qq.com>
2020-03-19 11:05:18 +0800

    启动优化

--- src/main/java/com/company/project/CompanyProjectApplication.java
+++ src/main/java/com/company/project/CompanyProjectApplication.java
@@ -1,16 +1,33 @@
 package com.company.project;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
 
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @MapperScan("com.company.project.mapper")
+@Slf4j
 public class CompanyProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CompanyProjectApplication.class, args);
+    public static void main(String[] args) throws Exception{
+        ConfigurableApplicationContext application = SpringApplication.run(CompanyProjectApplication.class, args);
+
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Login: \thttp://localhost:{}/login\n\t" +
+                        "Doc: \thttp://{}:{}/doc.html\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
     }
 
 }
