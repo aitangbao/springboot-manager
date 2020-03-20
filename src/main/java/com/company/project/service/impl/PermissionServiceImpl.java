99d51589f37e9fed29f0f3a1b9508a057c744724
wenbin <1104302665@qq.com>
2020-03-20 18:45:26 +0800

    优化

--- src/main/java/com/company/project/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/company/project/service/impl/PermissionServiceImpl.java
@@ -36,6 +36,7 @@ public class PermissionServiceImpl extends ServiceImpl<SysPermissionMapper, SysP
     private RolePermissionService rolePermissionService;
     @Autowired
     private SysPermissionMapper sysPermissionMapper;
+
     /**
      * 根据用户查询拥有的权限
      * 先查出用户拥有的角色
@@ -45,83 +46,86 @@ public class PermissionServiceImpl extends ServiceImpl<SysPermissionMapper, SysP
     @Override
     public List<SysPermission> getPermission(String userId) {
         List<String> roleIds = userRoleService.getRoleIdsByUserId(userId);
-        if(roleIds.isEmpty()){
+        if (roleIds.isEmpty()) {
             return null;
         }
-        List<String> permissionIds= rolePermissionService.getPermissionIdsByRoles(roleIds);
-        if (permissionIds.isEmpty()){
+        List<String> permissionIds = rolePermissionService.getPermissionIdsByRoles(roleIds);
+        if (permissionIds.isEmpty()) {
             return null;
         }
-        List<SysPermission> result=sysPermissionMapper.selectInfoByIds(permissionIds);
+        List<SysPermission> result = sysPermissionMapper.selectInfoByIds(permissionIds);
         return result;
     }
+
     /**
      * 新增菜单权限
      */
     @Override
     public SysPermission addPermission(PermissionAddReqVO vo) {
-        SysPermission sysPermission=new SysPermission();
-        BeanUtils.copyProperties(vo,sysPermission);
+        SysPermission sysPermission = new SysPermission();
+        BeanUtils.copyProperties(vo, sysPermission);
         verifyForm(sysPermission);
         sysPermission.setCreateTime(new Date());
-        int count=sysPermissionMapper.insert(sysPermission);
-        if (count!=1){
+        int count = sysPermissionMapper.insert(sysPermission);
+        if (count != 1) {
             throw new BusinessException(BaseResponseCode.OPERATION_ERRO);
         }
         return sysPermission;
     }
+
     /**
      * 操作后的菜单类型是目录的时候 父级必须为目录
      * 操作后的菜单类型是菜单的时候，父类必须为目录类型
      * 操作后的菜单类型是按钮的时候 父类必须为菜单类型
      */
-    private void verifyFormPid(SysPermission sysPermission){
-        SysPermission parent=sysPermissionMapper.selectById(sysPermission.getPid());
-        switch (sysPermission.getType()){
+    private void verifyFormPid(SysPermission sysPermission) {
+        SysPermission parent = sysPermissionMapper.selectById(sysPermission.getPid());
+        switch (sysPermission.getType()) {
             case 1:
-                if(parent!=null){
-                    if(parent.getType()!=1){
+                if (parent != null) {
+                    if (parent.getType() != 1) {
                         throw new BusinessException(BaseResponseCode.OPERATION_MENU_PERMISSION_CATALOG_ERROR);
                     }
-                }else if(!sysPermission.getPid().equals("0")){
-                        throw new BusinessException(BaseResponseCode.OPERATION_MENU_PERMISSION_CATALOG_ERROR);
-                    }
+                } else if (!sysPermission.getPid().equals("0")) {
+                    throw new BusinessException(BaseResponseCode.OPERATION_MENU_PERMISSION_CATALOG_ERROR);
+                }
                 break;
             case 2:
-                if(parent==null||parent.getType()!=1){
+                if (parent == null || parent.getType() != 1) {
                     throw new BusinessException(BaseResponseCode.OPERATION_MENU_PERMISSION_MENU_ERROR);
                 }
-                if(StringUtils.isEmpty(sysPermission.getUrl())){
+                if (StringUtils.isEmpty(sysPermission.getUrl())) {
                     throw new BusinessException(BaseResponseCode.OPERATION_MENU_PERMISSION_URL_NOT_NULL);
                 }
 
                 break;
             case 3:
-                if(parent==null||parent.getType()!=2){
+                if (parent == null || parent.getType() != 2) {
                     throw new BusinessException(BaseResponseCode.OPERATION_MENU_PERMISSION_BTN_ERROR);
                 }
-                if(StringUtils.isEmpty(sysPermission.getPerms())){
+                if (StringUtils.isEmpty(sysPermission.getPerms())) {
                     throw new BusinessException(BaseResponseCode.OPERATION_MENU_PERMISSION_URL_PERMS_NULL);
                 }
-                if(StringUtils.isEmpty(sysPermission.getUrl())){
+                if (StringUtils.isEmpty(sysPermission.getUrl())) {
                     throw new BusinessException(BaseResponseCode.OPERATION_MENU_PERMISSION_URL_NOT_NULL);
                 }
-                if(StringUtils.isEmpty(sysPermission.getMethod())){
+                if (StringUtils.isEmpty(sysPermission.getMethod())) {
                     throw new BusinessException(BaseResponseCode.OPERATION_MENU_PERMISSION_URL_METHOD_NULL);
                 }
                 break;
         }
     }
+
     /**
      * 编辑或者新增的时候检验
      */
-    private void verifyForm(SysPermission sysPermission){
+    private void verifyForm(SysPermission sysPermission) {
 
         verifyFormPid(sysPermission);
         /**
          * id 不为空说明是编辑
          */
-        if(!StringUtils.isEmpty(sysPermission.getId())) {
+        if (!StringUtils.isEmpty(sysPermission.getId())) {
             QueryWrapper queryWrapper = new QueryWrapper();
             queryWrapper.eq("pid", sysPermission.getId());
             List<SysPermission> list = sysPermissionMapper.selectList(queryWrapper);
@@ -131,6 +135,7 @@ public class PermissionServiceImpl extends ServiceImpl<SysPermissionMapper, SysP
         }
 
     }
+
     /**
      * 查询菜单权限详情
      */
@@ -139,38 +144,39 @@ public class PermissionServiceImpl extends ServiceImpl<SysPermissionMapper, SysP
 
         return sysPermissionMapper.selectById(permissionId);
     }
+
     /**
      * 更新菜单权限
      */
     @Override
     public void updatePermission(PermissionUpdateReqVO vo) {
 
-        SysPermission sysPermission=sysPermissionMapper.selectById(vo.getId());
-        if (null==sysPermission){
-            log.error("传入 的 id:{}不合法",vo.getId());
+        SysPermission sysPermission = sysPermissionMapper.selectById(vo.getId());
+        if (null == sysPermission) {
+            log.error("传入 的 id:{}不合法", vo.getId());
             throw new BusinessException(BaseResponseCode.DATA_ERROR);
         }
-        SysPermission update=new SysPermission();
-        BeanUtils.copyProperties(vo,update);
+        SysPermission update = new SysPermission();
+        BeanUtils.copyProperties(vo, update);
         /**
          * 只有类型变更
          * 或者所属菜单变更
          */
-        if(sysPermission.getType()!=vo.getType()||!sysPermission.getPid().equals(vo.getPid())){
+        if (sysPermission.getType().equals(vo.getType()) || !sysPermission.getPid().equals(vo.getPid())) {
             verifyForm(update);
         }
         update.setUpdateTime(new Date());
-        int count=sysPermissionMapper.updateById(update);
-        if(count!=1){
+        int count = sysPermissionMapper.updateById(update);
+        if (count != 1) {
             throw new BusinessException(BaseResponseCode.OPERATION_ERRO);
         }
         /**
          * 说明这个菜单权限 权鉴标识发生变化
          * 所有管理这个菜单权限用户将重新刷新token
          */
-        if(StringUtils.isEmpty(vo.getPerms())&&!sysPermission.getPerms().equals(vo.getPerms())){
+        if (StringUtils.isEmpty(vo.getPerms()) && !sysPermission.getPerms().equals(vo.getPerms())) {
             List<String> roleIds = rolePermissionService.getRoleIds(vo.getId());
-            if(!roleIds.isEmpty()){
+            if (!roleIds.isEmpty()) {
                 List<String> userIds = userRoleService.getUserIdsByRoleIds(roleIds);
 //                if(!userIds.isEmpty()){
 //                    for (String userId:userIds){
@@ -183,6 +189,7 @@ public class PermissionServiceImpl extends ServiceImpl<SysPermissionMapper, SysP
         }
 
     }
+
     /**
      * 删除菜单权限
      * 判断是否 有角色关联
@@ -192,18 +199,18 @@ public class PermissionServiceImpl extends ServiceImpl<SysPermissionMapper, SysP
     @Override
     public void deleted(String permissionId) {
         SysPermission sysPermission = sysPermissionMapper.selectById(permissionId);
-        if(null==sysPermission){
-            log.error("传入 的 id:{}不合法",permissionId);
+        if (null == sysPermission) {
+            log.error("传入 的 id:{}不合法", permissionId);
             throw new BusinessException(BaseResponseCode.DATA_ERROR);
         }
         QueryWrapper queryWrapper = new QueryWrapper();
         queryWrapper.eq("pid", permissionId);
         List<SysPermission> childs = sysPermissionMapper.selectList(queryWrapper);
-        if(!childs.isEmpty()){
+        if (!childs.isEmpty()) {
             throw new BusinessException(BaseResponseCode.ROLE_PERMISSION_RELATION);
         }
-        int count=sysPermissionMapper.deleteById(permissionId);
-        if(count!=1){
+        int count = sysPermissionMapper.deleteById(permissionId);
+        if (count != 1) {
             throw new BusinessException(BaseResponseCode.OPERATION_ERRO);
         }
         /**
@@ -211,26 +218,28 @@ public class PermissionServiceImpl extends ServiceImpl<SysPermissionMapper, SysP
          */
         rolePermissionService.removeByPermissionId(permissionId);
     }
+
     /**
      * 分页获取所有菜单权限
      */
     @Override
     public IPage<SysPermission> pageInfo(PermissionPageReqVO vo) {
 
-        Page page = new Page(vo.getPageNum(),vo.getPageSize());
+        Page page = new Page(vo.getPageNum(), vo.getPageSize());
         QueryWrapper queryWrapper = new QueryWrapper();
         queryWrapper.orderByAsc("order_num");
-        IPage<SysPermission> result =sysPermissionMapper.selectPage(page, queryWrapper);
-        if(!result.getRecords().isEmpty()){
-            for (SysPermission sysPermission:result.getRecords()){
-                SysPermission parent=sysPermissionMapper.selectById(sysPermission.getPid());
-                if (parent!=null){
+        IPage<SysPermission> result = sysPermissionMapper.selectPage(page, queryWrapper);
+        if (!result.getRecords().isEmpty()) {
+            for (SysPermission sysPermission : result.getRecords()) {
+                SysPermission parent = sysPermissionMapper.selectById(sysPermission.getPid());
+                if (parent != null) {
                     sysPermission.setPidName(parent.getName());
                 }
             }
         }
         return result;
     }
+
     /**
      * 获取所有菜单权限
      */
@@ -238,112 +247,119 @@ public class PermissionServiceImpl extends ServiceImpl<SysPermissionMapper, SysP
     public List<SysPermission> selectAll() {
         QueryWrapper queryWrapper = new QueryWrapper();
         queryWrapper.orderByAsc("order_num");
-        List<SysPermission> result =sysPermissionMapper.selectList(queryWrapper);
-        if(!result.isEmpty()){
-            for (SysPermission sysPermission:result){
-                SysPermission parent=sysPermissionMapper.selectById(sysPermission.getPid());
-                if (parent!=null){
+        List<SysPermission> result = sysPermissionMapper.selectList(queryWrapper);
+        if (!result.isEmpty()) {
+            for (SysPermission sysPermission : result) {
+                SysPermission parent = sysPermissionMapper.selectById(sysPermission.getPid());
+                if (parent != null) {
                     sysPermission.setPidName(parent.getName());
                 }
             }
         }
         return result;
     }
+
     /**
      * 获取权限标识
      */
     @Override
     public Set<String> getPermissionsByUserId(String userId) {
 
-        List<SysPermission> list=getPermission(userId);
-        Set<String> permissions=new HashSet<>();
-        if (null==list||list.isEmpty()){
+        List<SysPermission> list = getPermission(userId);
+        Set<String> permissions = new HashSet<>();
+        if (null == list || list.isEmpty()) {
             return null;
         }
-        for (SysPermission sysPermission:list){
-            if(!StringUtils.isEmpty(sysPermission.getPerms())){
+        for (SysPermission sysPermission : list) {
+            if (!StringUtils.isEmpty(sysPermission.getPerms())) {
                 permissions.add(sysPermission.getPerms());
             }
 
         }
         return permissions;
     }
+
     /**
      * 以树型的形式把用户拥有的菜单权限返回给客户端
      */
     @Override
     public List<PermissionRespNode> permissionTreeList(String userId) {
-        List<SysPermission> list=getPermission(userId);
-        return getTree(list,true);
+        List<SysPermission> list = getPermission(userId);
+        return getTree(list, true);
     }
+
     /**
      * 递归获取菜单树
      */
-    private List<PermissionRespNode> getTree(List<SysPermission> all,boolean type){
+    private List<PermissionRespNode> getTree(List<SysPermission> all, boolean type) {
 
-        List<PermissionRespNode> list=new ArrayList<>();
-        if (all==null||all.isEmpty()){
+        List<PermissionRespNode> list = new ArrayList<>();
+        if (all == null || all.isEmpty()) {
             return list;
         }
-        for(SysPermission sysPermission:all){
-            if(sysPermission.getPid().equals("0")){
-                PermissionRespNode permissionRespNode=new PermissionRespNode();
-                BeanUtils.copyProperties(sysPermission,permissionRespNode);
+        for (SysPermission sysPermission : all) {
+            if (sysPermission.getPid().equals("0")) {
+                PermissionRespNode permissionRespNode = new PermissionRespNode();
+                BeanUtils.copyProperties(sysPermission, permissionRespNode);
                 permissionRespNode.setTitle(sysPermission.getName());
 
-                if(type){
-                    permissionRespNode.setChildren(getChildExcBtn(sysPermission.getId(),all));
-                }else {
-                    permissionRespNode.setChildren(getChildAll(sysPermission.getId(),all));
+                if (type) {
+                    permissionRespNode.setChildren(getChildExcBtn(sysPermission.getId(), all));
+                } else {
+                    permissionRespNode.setChildren(getChildAll(sysPermission.getId(), all));
                 }
                 list.add(permissionRespNode);
             }
         }
         return list;
     }
+
     /**
      * 递归遍历所有
      */
-    private List<PermissionRespNode>getChildAll(String id,List<SysPermission> all){
+    private List<PermissionRespNode> getChildAll(String id, List<SysPermission> all) {
 
-        List<PermissionRespNode> list=new ArrayList<>();
-        for(SysPermission sysPermission:all){
-            if(sysPermission.getPid().equals(id)){
-                PermissionRespNode permissionRespNode=new PermissionRespNode();
-                BeanUtils.copyProperties(sysPermission,permissionRespNode);
+        List<PermissionRespNode> list = new ArrayList<>();
+        for (SysPermission sysPermission : all) {
+            if (sysPermission.getPid().equals(id)) {
+                PermissionRespNode permissionRespNode = new PermissionRespNode();
+                BeanUtils.copyProperties(sysPermission, permissionRespNode);
                 permissionRespNode.setTitle(sysPermission.getName());
-                permissionRespNode.setChildren(getChildAll(sysPermission.getId(),all));
+                permissionRespNode.setChildren(getChildAll(sysPermission.getId(), all));
                 list.add(permissionRespNode);
             }
         }
         return list;
     }
+
     /**
      * 只递归获取目录和菜单
      */
-    private List<PermissionRespNode>getChildExcBtn(String id,List<SysPermission> all){
+    private List<PermissionRespNode> getChildExcBtn(String id, List<SysPermission> all) {
 
-        List<PermissionRespNode> list=new ArrayList<>();
-        for(SysPermission sysPermission:all){
-            if(sysPermission.getPid().equals(id)&&sysPermission.getType()!=3){
-                PermissionRespNode permissionRespNode=new PermissionRespNode();
-                BeanUtils.copyProperties(sysPermission,permissionRespNode);
+        List<PermissionRespNode> list = new ArrayList<>();
+        for (SysPermission sysPermission : all) {
+            if (sysPermission.getPid().equals(id) && sysPermission.getType() != 3) {
+                PermissionRespNode permissionRespNode = new PermissionRespNode();
+                BeanUtils.copyProperties(sysPermission, permissionRespNode);
                 permissionRespNode.setTitle(sysPermission.getName());
-                permissionRespNode.setChildren(getChildExcBtn(sysPermission.getId(),all));
+                permissionRespNode.setChildren(getChildExcBtn(sysPermission.getId(), all));
                 list.add(permissionRespNode);
             }
         }
         return list;
     }
+
     /**
      * 获取所有菜单权限按钮
      */
     @Override
     public List<PermissionRespNode> selectAllByTree() {
 
-        List<SysPermission> list=selectAll();
-        return getTree(list,false);
+        List<SysPermission> list = selectAll();
+        return getTree(list, false);
     }
+
     /**
      * 获取所有的目录菜单树排除按钮
      * 因为不管是新增或者修改
@@ -355,22 +371,22 @@ public class PermissionServiceImpl extends ServiceImpl<SysPermissionMapper, SysP
     @Override
     public List<PermissionRespNode> selectAllMenuByTree(String permissionId) {
 
-        List<SysPermission> list=selectAll();
-        if(!list.isEmpty()&&!StringUtils.isEmpty(permissionId)){
-            for (SysPermission sysPermission:list){
-                if (sysPermission.getId().equals(permissionId)){
+        List<SysPermission> list = selectAll();
+        if (!list.isEmpty() && !StringUtils.isEmpty(permissionId)) {
+            for (SysPermission sysPermission : list) {
+                if (sysPermission.getId().equals(permissionId)) {
                     list.remove(sysPermission);
                     break;
                 }
             }
         }
-        List<PermissionRespNode> result=new ArrayList<>();
+        List<PermissionRespNode> result = new ArrayList<>();
         //新增顶级目录是为了方便添加一级目录
-        PermissionRespNode respNode=new PermissionRespNode();
+        PermissionRespNode respNode = new PermissionRespNode();
         respNode.setId("0");
         respNode.setTitle("默认顶级菜单");
         respNode.setSpread(true);
-        respNode.setChildren(getTree(list,true));
+        respNode.setChildren(getTree(list, true));
         result.add(respNode);
         return result;
     }
