36856f108f79fec37b7ce058a9b253b389bbcd3d
wenbin <1104302665@qq.com>
2020-03-27 18:02:04 +0800

    同步github

--- doc/company_project.sql
+++ doc/company_project.sql
@@ -58,7 +58,7 @@ CREATE TABLE `sys_generator` (
   `pid` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT '上级菜单',
   `gen_time` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin  COMMENT='代码生成';
 
 -- ----------------------------
 -- Records of sys_generator
