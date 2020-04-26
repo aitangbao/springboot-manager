d9c54db519f8788ff9e020b253aedccfe82827bd
wenbin <liwenbin@haier.com>
2020-04-26 09:02:51 +0800

    fix:代码生成页面分页错误

--- src/main/java/com/company/project/controller/SysGeneratorController.java
+++ src/main/java/com/company/project/controller/SysGeneratorController.java
@@ -1,10 +1,12 @@
 package com.company.project.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.company.project.common.aop.annotation.LogAnnotation;
 import com.company.project.service.PermissionService;
 import com.company.project.common.utils.DataResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import com.company.project.service.ISysGeneratorService;
 import com.company.project.entity.SysGenerator;
