8eb1cb06c8f335d3d25a31c091ebebb235e2b5bf
wenbin <liwenbin@haier.com>
2020-04-27 17:00:07 +0800

    字典添加/修改 校验

--- src/main/java/com/company/project/controller/SysDictController.java
+++ src/main/java/com/company/project/controller/SysDictController.java
@@ -13,11 +13,11 @@ import io.swagger.annotations.ApiParam;
 
 import java.util.Date;
 import java.util.List;
+
 import com.company.project.common.utils.DataResult;
 
 import com.company.project.entity.SysDictEntity;
 import com.company.project.service.SysDictService;
-
 
 
 /**
@@ -35,18 +35,25 @@ public class SysDictController {
 
 
     /**
-    * 跳转到页面
-    */
+     * 跳转到页面
+     */
     @GetMapping("/index/sysDict")
     public String sysDict() {
         return "sysdict/list";
-        }
+    }
 
     @ApiOperation(value = "新增")
     @PostMapping("sysDict/add")
     @RequiresPermissions("sysDict:add")
     @ResponseBody
-    public DataResult add(@RequestBody SysDictEntity sysDict){
+    public DataResult add(@RequestBody SysDictEntity sysDict) {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("type", sysDict.getType());
+        queryWrapper.eq("code", sysDict.getCode());
+        SysDictEntity q = sysDictService.getOne(queryWrapper);
+        if (q != null) {
+            return DataResult.fail("字典类型-字典码已存在");
+        }
         sysDict.setCreateTime(new Date());
         sysDictService.save(sysDict);
         return DataResult.success();
@@ -56,7 +63,7 @@ public class SysDictController {
     @DeleteMapping("sysDict/delete")
     @RequiresPermissions("sysDict:delete")
     @ResponseBody
-    public DataResult delete(@RequestBody @ApiParam(value = "id集合") List<String> ids){
+    public DataResult delete(@RequestBody @ApiParam(value = "id集合") List<String> ids) {
         sysDictService.removeByIds(ids);
         return DataResult.success();
     }
@@ -65,7 +72,16 @@ public class SysDictController {
     @PutMapping("sysDict/update")
     @RequiresPermissions("sysDict:update")
     @ResponseBody
-    public DataResult update(@RequestBody SysDictEntity sysDict){
+    public DataResult update(@RequestBody SysDictEntity sysDict) {
+
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("type", sysDict.getType());
+        queryWrapper.eq("code", sysDict.getCode());
+        SysDictEntity q = sysDictService.getOne(queryWrapper);
+        if (q != null && !q.getId().equals(sysDict.getId())) {
+            return DataResult.fail("字典类型-字典码已存在");
+        }
+
         sysDictService.updateById(sysDict);
         return DataResult.success();
     }
@@ -74,14 +90,14 @@ public class SysDictController {
     @PostMapping("sysDict/listByPage")
     @RequiresPermissions("sysDict:list")
     @ResponseBody
-    public DataResult findListByPage(@RequestBody SysDictEntity sysDict){
+    public DataResult findListByPage(@RequestBody SysDictEntity sysDict) {
         Page page = new Page(sysDict.getPage(), sysDict.getLimit());
         QueryWrapper queryWrapper = new QueryWrapper();
         //查询条件示例
         if (!StringUtils.isEmpty(sysDict.getName())) {
             queryWrapper.like("name", sysDict.getName());
         }
-        queryWrapper.orderByAsc("name","order_num");
+        queryWrapper.orderByAsc("name", "order_num");
         IPage<SysDictEntity> iPage = sysDictService.page(page, queryWrapper);
         return DataResult.success(iPage);
     }
