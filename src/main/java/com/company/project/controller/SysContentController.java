9d4d2ef1f3ae7ab80c34cae59991fed1a4d820d3
wenbin <liwenbin@haier.com>
2020-06-18 17:50:12 +0800

    优化

--- src/main/java/com/company/project/controller/SysContentController.java
+++ src/main/java/com/company/project/controller/SysContentController.java
@@ -1,8 +1,6 @@
 package com.company.project.controller;
 
-import com.company.project.common.exception.BusinessException;
 import io.swagger.annotations.Api;
-import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
@@ -14,16 +12,11 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.company.project.common.utils.DataResult;
-
 import com.company.project.entity.SysContentEntity;
 import com.company.project.service.SysContentService;
-import org.springframework.web.multipart.MultipartFile;
-import sun.misc.BASE64Encoder;
 
 
 /**
