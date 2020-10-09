3b3c3f4c93486a8b101520a86e1c7445a48719b5
wenbin <liwenbin@haier.com>
2020-10-09 14:04:15 +0800

    优化数据权限逻辑

--- src/main/java/com/company/project/common/aop/aspect/DataScopeAspect.java
+++ src/main/java/com/company/project/common/aop/aspect/DataScopeAspect.java
@@ -74,15 +74,21 @@ public class DataScopeAspect {
     protected void handleDataScope(final JoinPoint joinPoint) {
         //获取当前登陆人
         String id = sessionService.getCurrentUserId();
-        //获取当前登陆人角色
+        //获取当前登陆人角色, 如果无角色, 那么不限制
         List<SysRole> sysRoles = roleService.getRoleInfoByUserId(id);
         if (CollectionUtils.isEmpty(sysRoles) || sysRoles.size() == 0) {
             return;
         }
+        //角色未配置数据权限范围, 那么不限制
         List<SysRole> list = sysRoles.parallelStream().filter(one -> null != one.getDataScope()).collect(Collectors.toList());
         if (CollectionUtils.isEmpty(list) || list.size() == 0) {
             return;
         }
+        //如果存在某角色配置了全部范围， 那么不限制
+        if (list.stream().anyMatch(sysRole -> DATA_SCOPE_ALL.equals(sysRole.getDataScope()))) {
+            return;
+        }
+        //获取绑定的人
         List<String> userIds = this.getUserIdsByRoles(list, id);
         Object params = joinPoint.getArgs()[0];
         if (params instanceof BaseEntity) {
@@ -99,35 +105,31 @@ public class DataScopeAspect {
         LinkedList<Object> deptlist = new LinkedList<>();
         //返回的用户ids
         LinkedList<Object> userIdList = new LinkedList<>();
-        //是否拥有查看所有数据标志
-        AtomicReference<Boolean> isAll = new AtomicReference<>(false);
         //根据数据权限范围分组， 不同的数据范围不同的逻辑处理
         Map<Integer, List<SysRole>> dataScopeMap = sysRoles.parallelStream().collect(Collectors.groupingBy(SysRole::getDataScope));
         dataScopeMap.forEach((k, v) -> {
-            if (DATA_SCOPE_ALL.equals(k)) {
-                //全部
-                isAll.set(true);
-                userIdList.addAll(userService.listObjs(Wrappers.<SysUser>lambdaQuery().select(SysUser::getId)));
-            } else if (DATA_SCOPE_CUSTOM.equals(k) && !isAll.get()) {
+
+            if (DATA_SCOPE_CUSTOM.equals(k)) {
                 //自定义
                 List<String> list = dataScopeMap.get(k).parallelStream().map(SysRole::getId).collect(Collectors.toList());
                 deptlist.addAll(sysRoleDeptService.listObjs(Wrappers.<SysRoleDeptEntity>lambdaQuery().select(SysRoleDeptEntity::getDeptId).in(SysRoleDeptEntity::getRoleId, list)));
-            } else if (DATA_SCOPE_DEPT_AND_CHILD.equals(k) && !isAll.get()) {
+            } else if (DATA_SCOPE_DEPT_AND_CHILD.equals(k)) {
+                //本部门及以下
                 SysDept sysDept = deptService.getById(sysUser.getDeptId());
                 if (StringUtils.isNotBlank(sysDept.getDeptNo())) {
-                    //本部门及以下
-                    List deptIds = deptService.listObjs(Wrappers.<SysDept>lambdaQuery().select(SysDept::getId).like(SysDept::getRelationCode, sessionService.getCurrentDeptNo()));
+                    List deptIds = deptService.listObjs(Wrappers.<SysDept>lambdaQuery().select(SysDept::getId).like(SysDept::getRelationCode, sysDept.getDeptNo()));
                     List<SysDept> deptList = deptService.listByIds(deptIds);
                     deptList.parallelStream().forEach(one -> {
                         deptlist.addAll(deptService.listObjs(Wrappers.<SysDept>lambdaQuery().select(SysDept::getId).like(SysDept::getRelationCode, one.getDeptNo())));
                     });
                 }
-            } else if (DATA_SCOPE_DEPT.equals(k) && !isAll.get()) {
-                if (StringUtils.isNotBlank(sessionService.getCurrentDeptId())) {
-                    //本部门
-                    deptlist.add(sessionService.getCurrentDeptId());
+            } else if (DATA_SCOPE_DEPT.equals(k)) {
+                //本部门
+                SysDept sysDept = deptService.getById(sysUser.getDeptId());
+                if (StringUtils.isNotBlank(sysDept.getId())) {
+                    deptlist.add(sysDept.getId());
                 }
-            } else if (DATA_SCOPE_DEPT_SELF.equals(k) && !isAll.get()) {
+            } else if (DATA_SCOPE_DEPT_SELF.equals(k)) {
                 //自己
                 userIdList.add(userId);
             }
