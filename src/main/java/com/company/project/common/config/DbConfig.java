fef5406271277cb7fbca8e70e413a6951710e020
yangjie <yangjie@trusfort.com>
2021-01-04 09:55:44 +0800

    .fix:优化数据库类型xml文件

--- src/main/java/com/company/project/common/config/DbConfig.java
+++ src/main/java/com/company/project/common/config/DbConfig.java
@@ -3,9 +3,6 @@ package com.company.project.common.config;
 import com.company.project.common.exception.BusinessException;
 import com.company.project.common.utils.Constant;
 import com.company.project.mapper.GeneratorMapper;
-import com.company.project.mapper.SysGeneratorMysqlMapper;
-import com.company.project.mapper.SysGeneratorOracleMapper;
-import com.company.project.mapper.SysGeneratorSqlServerMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,24 +22,25 @@ public class DbConfig {
 
     @Value("${project.database}")
     private String database;
-    @Resource
-    private SysGeneratorMysqlMapper sysGeneratorMysqlMapper;
-    @Resource
-    private SysGeneratorOracleMapper sysGeneratorOracleMapper;
-    @Resource
-    private SysGeneratorSqlServerMapper sysGeneratorSqlServerMapper;
 
-    @Bean
-    @Primary
-    public GeneratorMapper getGeneratorMapper() {
-        if (Constant.DB_TYPE_MYSQL.equalsIgnoreCase(database)) {
-            return sysGeneratorMysqlMapper;
-        } else if (Constant.DB_TYPE_ORACLE.equalsIgnoreCase(database)) {
-            return sysGeneratorOracleMapper;
-        } else if (Constant.DB_TYPE_SQL_SERVER.equalsIgnoreCase(database)) {
-            return sysGeneratorSqlServerMapper;
-        } else {
-            throw new BusinessException("不支持当前数据库：" + database);
-        }
-    }
+
+
+
+
+    //@Bean
+    //@Primary
+    //public GeneratorMapper getGeneratorMapper() {
+    //
+    //
+    //
+    //    if (Constant.DB_TYPE_MYSQL.equalsIgnoreCase(database)) {
+    //        return sysGeneratorMysqlMapper;
+    //    } else if (Constant.DB_TYPE_ORACLE.equalsIgnoreCase(database)) {
+    //        return sysGeneratorOracleMapper;
+    //    } else if (Constant.DB_TYPE_SQL_SERVER.equalsIgnoreCase(database)) {
+    //        return sysGeneratorSqlServerMapper;
+    //    } else {
+    //        throw new BusinessException("不支持当前数据库：" + database);
+    //    }
+    //}
 }
