7c014b20840660d43b1e2ce975bb31686339d5dd
liwenbin <liwenbin@haier.com>
2021-01-06 13:10:56 +0800

    MyBatisPlusConfig 优化

--- src/main/java/com/company/project/common/config/MyBatisPlusConfig.java
+++ src/main/java/com/company/project/common/config/MyBatisPlusConfig.java
@@ -1,9 +1,6 @@
 package com.company.project.common.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,13 +14,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyBatisPlusConfig {
     /**
-     * 配置mybatis-plus 拦截器
+     * 配置mybatis-plus 分页查件
      */
     @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        //添加分页拦截器
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        return interceptor;
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
     }
 }
